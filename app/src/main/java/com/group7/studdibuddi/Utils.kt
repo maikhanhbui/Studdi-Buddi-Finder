6096508a44111d127098b227780b25fd90334b04
BillyShi <lsa84@sfu.ca>
2023-11-16 20:49:08 -0800

    init commit

---
+++ app/src/main/java/com/group7/studdibuddi/Utils.kt
@@ -0,0 +1,17 @@
+package com.group7.studdibuddi
+
+import android.app.Activity
+
+object Utils {
+
+    // Connect to database, use local cache if failed
+    fun establishConnection(activity: Activity){
+        
+    }
+
+    // Room database local access
+    fun restoreLocalData(activity: Activity){
+
+    }
+
+}
