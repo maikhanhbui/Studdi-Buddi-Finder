f92131d96c477d1fb081d5add8cf628210ae8183
abielk <abielk@sfu.ca>
2023-11-18 20:28:08 -0800

    DB create account & DB sign in

--- app/src/main/java/com/group7/studdibuddi/DatabaseUtil.kt
+++ app/src/main/java/com/group7/studdibuddi/DatabaseUtil.kt
@@ -1,8 +1,11 @@
 package com.group7.studdibuddi
 
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.net.ConnectivityManager
+import android.util.Log
+import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -12,11 +15,56 @@ object DatabaseUtil {
     lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
 
+    fun createAccount(
+        activity: Activity,
+        email: String,
+        password: String,
+        callback: (Boolean) -> Unit)
+    {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(activity) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    callback(true)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        activity,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    callback(false)
+                }
+            }
+    }
 
-    fun userLogin(email: String, passWord: String): Boolean{
-        // TODO: given email, password log the user in, NOTE: register page required first to test it
-        // TODO: return true is login successful
-        return true
+    fun signIn(
+        activity: Activity,
+        email: String,
+        password: String,
+        callback: (Boolean) -> Unit)
+    {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(activity) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    callback(true)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        activity,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    callback(false)
+                }
+            }
     }
 
     // Room database local access
