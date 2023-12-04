eebd8c2c276c68e18c5ebc25404de985aa05d4cb
kmbui <mkb18@sfu.ca>
2023-12-03 23:26:12 -0800

    some reformatting

--- app/src/main/java/com/group7/studdibuddi/MySessionActivity.kt
+++ app/src/main/java/com/group7/studdibuddi/MySessionActivity.kt
@@ -1,6 +1,5 @@
 package com.group7.studdibuddi
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,19 +7,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.google.firebase.database.FirebaseDatabase
 import com.group7.studdibuddi.session.SessionUtil
 import com.group7.studdibuddi.databinding.ActivityMySessionBinding
 import com.group7.studdibuddi.session.Session
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class MySessionActivity : ComponentActivity() {
     private lateinit var binding: ActivityMySessionBinding
@@ -31,20 +21,20 @@ class MySessionActivity : ComponentActivity() {
 
 
         if (SessionUtil.selectedSession == null || DatabaseUtil.currentUser == null){
-            Toast.makeText(this, "Session Error", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.session_error), Toast.LENGTH_SHORT).show()
             Log.d("database", "Chat session data error")
             finish()
         }
         val curSession = SessionUtil.selectedSession!!
 
         // Draw the info
-        binding.textViewSessionName.text = "Session Name: ${curSession.sessionName}"
-        binding.textViewSessionLocation.text = "Location Number: ${curSession.location}"
-        binding.textViewCourseId.text = "Course Number: ${curSession.courseId}"
-        binding.textViewSessionDescription.text = "Description: ${curSession.description}"
+        binding.textViewSessionName.text = "${getString(R.string.session_name_2)}: ${curSession.sessionName}"
+        binding.textViewSessionLocation.text = "${getString(R.string.location_number)}: ${curSession.location}"
+        binding.textViewCourseId.text = "${getString(R.string.course_number)}: ${curSession.courseId}"
+        binding.textViewSessionDescription.text = "${getString(R.string.description_2)}: ${curSession.description}"
 
-        binding.textViewStartTime.text = "Start Time: ${Util.timeStampToTimeString(curSession.startTime)}"
-        binding.textViewEndTime.text = "End Time: ${Util.timeStampToTimeString(curSession.endTime)}"
+        binding.textViewStartTime.text = "${getString(R.string.start_time)}: ${Util.timeStampToTimeString(curSession.startTime)}"
+        binding.textViewEndTime.text = "${getString(R.string.end_time)}: ${Util.timeStampToTimeString(curSession.endTime)}"
 
         binding.buttonChat.setOnClickListener{
             val intent = Intent(this, ChatActivity::class.java)
@@ -53,7 +43,7 @@ class MySessionActivity : ComponentActivity() {
 
         // Get Group Member names
         SessionUtil.joinedUser(curSession.usersJoined, curSession.ownerId) { usernames ->
-            binding.textViewGroup.text = "Group Members: ${usernames.joinToString(", ")}"
+            binding.textViewGroup.text = "${getString(R.string.group_members)}: ${usernames.joinToString(", ")}"
         }
 
         // Check if the current user is the owner
